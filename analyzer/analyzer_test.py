4acdb32991a8ac89dd6ac0be829cc114d2e6adca
Alex Rodriguez <arodriguez99@protonmail.com>
2023-07-23 20:18:30 -0400

    Removes duplicate pytest code

--- analyzer/analyzer_test.py
+++ analyzer/analyzer_test.py
@@ -1,5 +1,6 @@
 """analyzer_test.py: tests for analyzer.py"""
 # pylint: disable=missing-function-docstring
+from pathlib import Path
 import pytest
 import yaml
 
@@ -12,61 +13,9 @@ def analyzer_fixture():
     return Analyzer()
 
 
-def test_check_for_3p_actions_without_hash(analyzer) -> None:
-    with open("actions/action-using-configure-aws-creds-non-oidc-auth.yml", "r", encoding="utf-8") as action:
-        action_dict = yaml.safe_load(action)
-        assert analyzer.run_checks(action=action_dict) is True
-
-
-def test_check_for_script_injection(analyzer) -> None:
-    with open("actions/action-with-dangerous-gh-context-variables.yml", "r", encoding="utf-8") as action:
-        action_dict = yaml.safe_load(action)
-        assert analyzer.run_checks(action=action_dict) is True
-
-
-def test_check_for_unsecure_commands(analyzer) -> None:
-    with open("actions/action-with-unsecure-command-env-var.yml", "r", encoding="utf-8") as action:
-        action_dict = yaml.safe_load(action)
-        assert analyzer.run_checks(action=action_dict) is True
-
-
-def test_check_for_pull_request_target(analyzer) -> None:
-    with open("actions/action-with-pull-request-target.yml", "r", encoding="utf-8") as action:
-        action_dict = yaml.safe_load(action)
-        assert analyzer.run_checks(action=action_dict) is True
-
-
-def test_check_for_inline_script(analyzer) -> None:
-    with open("actions/action-with-inline-script.yml", "r", encoding="utf-8") as action:
-        action_dict = yaml.safe_load(action)
-        assert analyzer.run_checks(action=action_dict) is True
-
-
-def test_check_for_cache_action_usage(analyzer) -> None:
-    with open("actions/action-using-github-cache.yml", "r", encoding="utf-8") as action:
-        action_dict = yaml.safe_load(action)
-        assert analyzer.run_checks(action=action_dict) is True
-
-
-def test_check_for_self_hosted_runners(analyzer) -> None:
-    with open("actions/action-using-self-hosted-runners.yml", "r", encoding="utf-8") as action:
-        action_dict = yaml.safe_load(action)
-        assert analyzer.run_checks(action=action_dict) is True
-
-    with open("actions/action-using-self-hosted-runner-referenced-by-group.yml", "r", encoding="utf-8") as action:
-        action_dict = yaml.safe_load(action)
-        assert analyzer.run_checks(action=action_dict) is True
-
-
-def test_check_for_dangerous_write_permissions(analyzer) -> None:
-    with open("actions/action-with-write-permissions-all-jobs.yml", "r", encoding="utf-8") as action:
-        action_dict = yaml.safe_load(action)
-        assert analyzer.run_checks(action=action_dict) is True
-
-    with open("actions/action-with-write-permissions-one-job.yml", "r", encoding="utf-8") as action:
-        action_dict = yaml.safe_load(action)
-        assert analyzer.run_checks(action=action_dict) is True
-
-    with open("actions/action-with-write-all-permissions.yml", "r", encoding="utf-8") as action:
-        action_dict = yaml.safe_load(action)
-        assert analyzer.run_checks(action=action_dict) is True
+def test_all_checks(analyzer):
+    dir_ = Path("./actions")
+    for file_ in dir_.iterdir():
+        with open(file_, "r", encoding="utf-8") as action:
+            action_dict = yaml.safe_load(action)
+            assert analyzer.run_checks(action=action_dict) is True
