ec934d58d18767e9275d2150941cc5ec88d4d2ce
Alexis Rodriguez <arodriguez99@protonmail.com>
2023-08-07 10:45:52 -0400

    Removes unnecessary lines from example workflow in README.md and updates version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ghast-scanner',
-    version='1.5.7',
+    version='1.6.7',
     packages=find_packages(),
     install_requires=[
         'iniconfig==2.0.0',
@@ -15,7 +15,7 @@ setup(
     author_email='arodriguez99@protonmail.com',
     description='Analyze the security posture of your GitHub Action',
     url='https://github.com/bin3xish477/ghast',
-    classifiers = [
+    classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
